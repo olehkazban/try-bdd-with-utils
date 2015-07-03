697e1ea89fb745a18ef99e166c1faa6426be6c01
Oleh Kazban <oleh.kazban@globallogic.com>
2015-07-03 10:48:05 +0300

    Initial commit

--- test.js
+++ test.js
@@ -4,11 +4,27 @@ var utils = require('./utils'),
 describe('Utils', function() {
 
 	describe('#sort()', function() {
-		it('should sort given array of numbers with ascending ordering', function() {
-			expect(utils.sort([2, 1, 3, 0])).to.equal([0, 1, 2, 3]);
+		it('should sort given array of numbers with ascending ordering (default - without comparator)', function() {
+			expect(utils.sort([2, 1, 3, 0]).join()).to.equal([0, 1, 2, 3].join());
 		});
+	});
 
-		//TODO: don't forget about custom comparator function
+	describe('#sort()', function() {
+		it('should sort given array of numbers with ascending ordering (with equal comparator: 0)', function() {
+			expect(utils.sort([2, 1, 1, 0], function(a, b){ return a == b; }).join()).to.equal([0, 1, 1, 2].join());
+		});
+	});
+
+	describe('#sort()', function() {
+		it('should sort given array of numbers with ascending ordering (with ascending comparator: 1)', function() {
+			expect(utils.sort([2, 1, 3, 0], function(a, b){ return b - a; }).join()).to.equal([0, 1, 2, 3].join());
+		});
+	});
+
+	describe('#sort()', function() {
+		it('should sort given array of numbers with descending ordering (with descending comparator: -1)', function() {
+			expect(utils.sort([2, 1, 3, 0], function(a, b){ return a - b; }).join()).to.equal([3, 2, 1, 0].join());
+		});
 	});
 
 	describe('#capitalize()', function() {
@@ -17,6 +33,52 @@ describe('Utils', function() {
 		});
 	});
 
-	//TODO: Add your specs for other methods
+	describe('#capitalize()', function() {
+		it('should make first letter of given string upper case', function() {
+			expect(utils.capitalize('JUST DO IT!')).to.equal('Just do it!');
+		});
+	});
+
+  describe('#camelize()', function() {
+    it('should camelize given string', function() {
+      expect(utils.camelize('just do it!')).to.equal('JustDoIt');
+    });
+  });
+
+  describe('#camelize()', function() {
+    it('should camelize given array of string', function() {
+      expect(utils.camelize(['just','do','it!'])).to.equal('JustDoIt');
+    });
+  });
+
+  describe('#trim()', function() {
+    it('should cut of any count of spaces from the beginning and from the end of the string', function() {
+    });
+  });
+
+  describe('#reverse()', function() {
+    it('should reverses a specified list', function() {
+    });
+  });
+
+  describe('#map()', function() {
+    it('should change each list element by applying handler', function() {
+    });
+  });
+
+  describe('#groupBy()', function() {
+    it('should group some input sequence of element by some rule', function() {
+    });
+  });
+
+  describe('#once()', function() {
+    it('should create a version of the function that can only be called one time', function() {
+    });
+  });
+
+  describe('#debounce()', function() {
+    it('should create and return a new debounced version of the passed function', function() {
+    });
+  });
 
 });
--- utils.js
+++ utils.js
@@ -1,100 +1,149 @@
 module.exports = {
- 
-    /**
-     * Sort given array by provided rule in comparator function
-     * @param {Array} list
-     * @param {Function} comparator
-     */
- 
-    sort:function (list, comparator) {
-        return [];
-    },
- 
-    /**
-     * Make first letter of given string upper case
-     * @param {String} string
-     * @return {String} capitalized string
-     */
- 
-    capitalize:function (string) {
-        return "";
-    },
- 
-    /**
-     * Camelize given string or array of string
-     * @param {Array|String} sequence
-     * @return {String} capitalized string
-     */
- 
-    camelize:function (sequence) {
-        return "";
-    },
- 
-    /**
-     * Cut of any count of spaces from the beginning and from the end of the string
-     * @param {String} str
-     * @return {String}
-     */
- 
-    trim:function (str) {
-        return "";
-    },
 
-    /**
-     * Reverses a specified list.
-     * @param {Array} list - a list to be reversed, may be empty.
-     * @return {Array} - the same instance of list but reverted
-     */
- 
-    reverse:function (list) {
-        return [];
-    },
- 
-    /**
-     *  Change each list's element by applying handler
-     *  @params {Array|Object} list - input sequence
-     *  @params {Function} iterator  - some rule which changes each element
-     *  @return {Array} new list with changes elements
-     */
- 
-    map:function (list, iterator) {
-        return [];
-    },
- 
-    /**
-     * Group some input sequence of element by some rule
-     * @param {Array} list - input sequence
-     * @param {Function} iterator -  provide group id for each element
-     * @return {Object} object of group id properties which point to arrays of element from input sequence
-     */
- 
-    groupBy:function (list, iterator) {
-        return {};
-    },
+  /**
+   * Sort given array by provided rule in comparator function
+   * @param {Array} list
+   * @param {Function} comparator
+   */
 
-    /**
-     * Creates a version of the function that can only be called one time. 
-     * Repeated calls to the modified function will have no effect. 
-     * @param {Function} func - your target function
-     * @return {Function} new  function which could be invoked only once
-     */
+  sort: function (list, comparator) {
+    var condition;
 
-    once: function(func){
-        return;
-    }, 
-
-
-    /**
-     * Creates and returns a new debounced version of the passed function 
-     * which will postpone its execution until after wait milliseconds 
-     * have elapsed since the last time it was invoked. 
-     * @param {Function} func - your target function
-     * @param {Number} wait -  milliseconds have elapsed since the last time it was invoked
-     * @return {Function} new debounced version of the passed function
-     */
-
-    debounce: function(func, wait){
-        return;
+    if (comparator) {
+      condition = comparator(10, 20);
     }
- 
-};
+
+    if (condition == undefined || condition >= 0) {
+      for (var barrier = list.length - 1; barrier >= 0; barrier--) {
+        for (var count = 0; count < barrier; count++) {
+          if (list[count] > list[count + 1]) {
+            var tmp = list[count];
+            list[count] = list[count + 1];
+            list[count + 1] = tmp;
+          }
+        }
+      }
+    } else if (condition < 0) {
+      for (var barrier = 0; barrier < list.length; barrier++) {
+        for (var count = barrier; count >= 0; count--) {
+          if (list[count] < list[count + 1]) {
+            var tmp = list[count];
+            list[count] = list[count + 1];
+            list[count + 1] = tmp;
+          }
+        }
+      }
+    }
+
+    return list;
+  },
+
+  /**
+   * Make first letter of given string upper case
+   * @param {String} string
+   * @return {String} capitalized string
+   */
+
+  capitalize: function (string) {
+    string = string.toLowerCase();
+    string = string.replace(/^[a-z]/, function (m) {
+      return m.toUpperCase();
+    });
+    return string;
+  }
+  ,
+
+  /**
+   * Camelize given string or array of string
+   * @param {Array|String} sequence
+   * @return {String} capitalized string
+   */
+
+  camelize: function (sequence) {
+    var string = '';
+
+    if (sequence instanceof Array) {
+      for (var counter = 0; counter < sequence.length; counter++) {
+        if (/[a-zA-Zà-ÿÀ-ß]/.test(sequence[counter])) {
+
+        }
+      }
+    } else if (sequence instanceof String) {
+    }
+
+
+  },
+
+  /**
+   * Cut of any count of spaces from the beginning and from the end of the string
+   * @param {String} str
+   * @return {String}
+   */
+
+  trim: function (str) {
+    return "";
+  }
+  ,
+
+  /**
+   * Reverses a specified list.
+   * @param {Array} list - a list to be reversed, may be empty.
+   * @return {Array} - the same instance of list but reverted
+   */
+
+  reverse: function (list) {
+    return [];
+  }
+  ,
+
+  /**
+   *  Change each list's element by applying handler
+   *  @params {Array|Object} list - input sequence
+   *  @params {Function} iterator  - some rule which changes each element
+   *  @return {Array} new list with changes elements
+   */
+
+  map: function (list, iterator) {
+    return [];
+  }
+  ,
+
+  /**
+   * Group some input sequence of element by some rule
+   * @param {Array} list - input sequence
+   * @param {Function} iterator -  provide group id for each element
+   * @return {Object} object of group id properties which point to arrays of element from input sequence
+   */
+
+  groupBy: function (list, iterator) {
+    return {};
+  }
+  ,
+
+  /**
+   * Creates a version of the function that can only be called one time.
+   * Repeated calls to the modified function will have no effect.
+   * @param {Function} func - your target function
+   * @return {Function} new  function which could be invoked only once
+   */
+
+  once: function (func) {
+    return;
+  }
+  ,
+
+  /**
+   * Creates and returns a new debounced version of the passed function
+   * which will postpone its execution until after wait milliseconds
+   * have elapsed since the last time it was invoked.
+   * @param {Function} func - your target function
+   * @param {Number} wait -  milliseconds have elapsed since the last time it was invoked
+   * @return {Function} new debounced version of the passed function
+   */
+
+  debounce: function (func, wait) {
+    return;
+  }
+
+}
+;
